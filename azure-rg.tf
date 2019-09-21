6bd6167135f060291f419dc62931d68a9adf0cca
sbirudar <52947502+sbirudar@users.noreply.github.com>
2019-09-21 16:16:06 +0530

    Update azure-rg.tf

--- azure-rg.tf
+++ azure-rg.tf
@@ -1,4 +1,28 @@
 
+variable "sid" {
+  default = "groupc"
+}
+
+variable "cid" {
+  default = "groupc"
+}
+
+variable "cs" {
+  default = "groupc"
+}
+
+
+variable "tid" {
+  default = "groupc"
+}
+
+provider "azurerm" {
+    subscription_id = "${var.sid}"
+    client_id       = "${var.cid}"
+    client_secret   = "${var.cs}"
+    tenant_id       = "${var.tid}"
+}
+
 
 resource "azurerm_resource_group" "myterraformgroup" {
     name     = "eurgdemo"
